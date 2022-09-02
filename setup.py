d7868e905452b0b5b87d8966175a129d498cabae
1901Analytics <102546764+1901Analytics@users.noreply.github.com>
2022-09-02 10:06:05 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='data3500',
+      version='0.0.3',
+      description='Module for Data 3500 MTSU Course',
+      url='https://github.com/1901Analytics/DATA3500',
+      author='Jeffrey Stark',
+      author_email='jeff.stark@mtsu.edu',
+      packages=['course_solutions'])
